8c935392a4f03b8ba8dfa68ae89859e8e256ed46
Christoph Suessmeier <mail@suessmeier.net>
2023-09-01 17:48:46 +0200

    added leaderboard route incl. pagination

--- backend/api/controllers/fightsController.js
+++ backend/api/controllers/fightsController.js
@@ -1,7 +1,19 @@
 import asyncHandler from "express-async-handler";
+import Fight from "../schema/fightSchema.js";
 
 const getLeaderboard = asyncHandler(async (req, res, next) => {
-  res.status(200).send("Leaderboard requested");
+  const { n = 10 } = req.query;
+  const { page = 1 } = req.query;
+
+  console.log("n", n, "page", page);
+
+  const results = await Fight.find()
+    .sort("-createdAt")
+    .skip(startAt)
+    .limit(n)
+    .exec();
+
+  res.status(200).json(results);
 });
 
 const postNewFight = asyncHandler(async (req, res, next) => {

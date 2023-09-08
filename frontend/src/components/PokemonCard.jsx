2d8b5859c42365c31ede99658fa4b2512031b59c
Christoph Suessmeier <mail@suessmeier.net>
2023-09-08 17:49:55 +0200

    fixed pokemon hook, added filterable list for pokemon selection

--- frontend/src/components/PokemonCard.jsx
+++ frontend/src/components/PokemonCard.jsx
@@ -1,18 +1,31 @@
-// frontend/src/components/PokemonCard.jsx
+/* eslint-disable react/prop-types */
 
-// ... (imports and other stuff)
+import { Fragment } from "react";
+import { createReadableStatsArray } from "../utils/pokemonUtil";
 
-const PokemonCard = ({ pokemon }) => {
-    return (
-      <div className="pokemon-card min-w-[200px] max-w-[250px] border border-opacity-40 rounded-lg p-2">
-        <img className="w-full h-1/2 object-cover rounded-t-lg" src={pokemon.sprites.front} alt={pokemon.name} />
-        <div className="text-center p-2">
-          <p className="font-bold">{pokemon.name}</p>
-          {/* Other stats */}
+const PokemonCard = ({ pokemon, handleSelection }) => {
+  const stats = createReadableStatsArray(pokemon);
+
+  return (
+    <button onClick={() => handleSelection(pokemon)}>
+      <div className="pokemon-card flex min-w-[200px] max-w-[250px] flex-col gap-2 rounded-lg border border-opacity-40 bg-white">
+        <img
+          className="h-1/2 w-full rounded-t-lg object-cover"
+          src={pokemon.sprites.artwork}
+          alt={pokemon.name}
+        />
+        <div className="text-center font-bold">{pokemon.name}</div>
+        <div className="grid grid-cols-[1fr_max-content] justify-center gap-x-4 gap-y-2 px-4">
+          {stats.map((stat) => (
+            <Fragment key={stat.name}>
+              <div>{stat.name}</div>
+              <div>{stat.value}</div>
+            </Fragment>
+          ))}
         </div>
       </div>
-    );
-  };
-  
-  export default PokemonCard;
-  
+    </button>
+  );
+};
+
+export default PokemonCard;
--- frontend/src/pages/poke_select_page.jsx
+++ frontend/src/pages/poke_select_page.jsx
@@ -1,64 +1,18 @@
-import { motion } from 'framer-motion'
-import { useEffect, useRef, useState } from 'react'
-import { Link } from 'react-router-dom'
+import FilteredList from "../components/FilteredList";
 
 const PokemonSelectionPage = () => {
-  const [pokemons, setPokemons] = useState([]);
-  const carouselRef = useRef()
-
-  useEffect(() => {
-    fetch('/initial_Load') // Replace with your actual backend API endpoint
-      .then(response => response.json())
-      .then(data => setPokemons(data))
-      .catch(error => console.error('Error:', error));
-    if (carouselRef.current) {
-      const resizeObserver = new ResizeObserver((entries) => {
-        for (let entry of entries) {
-          const width = entry.target.scrollWidth - entry.target.offsetWidth;
-          console.log('Updated width:', width);
-        }
-      });
-
-      resizeObserver.observe(carouselRef.current);
-   
-      return () => resizeObserver.disconnect();
-    }
-  }, [carouselRef.current]);
-
-  console.log('Rendering PokemonSelectionPage with pokemons:', pokemons); // Log the pokemons prop
+  const handleSelection = (pokemon) => {
+    alert(`Player selected ${pokemon.name}`);
+  };
 
   return (
-    pokemons && pokemons.length > 0 && (
-      <motion.div className='carousel-container' ref={carouselRef} drag='x'>
-        <div className='inner-carousel flex cursor-grab'>
-          {pokemons.map((pokemon) => {
-            console.log('Rendering pokemon:', pokemon); // Log each pokemon
-            return (
-              <motion.div
-                key={pokemon.id}
-                className='item min-h-[30rem] min-w-[30rem] w-[300px] p-[90px] transition-all duration-300 ease-in-out border-none mb-[30px] overflow-hidden'
-                whileHover={{ scale: 1.05, transition: { duration: 0.3 } }}
-              >
-                <Link to={`/pokemon/${pokemon.id}`}>
-                  <div className='relative h-full'>
-                    <img 
-                      className='w-full h-full rounded-2xl border-double border-opacity-20 shadow-md object-cover shadow-lg border-double rounded-2xl'
-                      src={pokemon.sprites.front_default} // Assumes the pokemon object has a 'sprites' property with a 'front_default' subproperty for the image
-                      alt={pokemon.name} // Assumes the pokemon object has a 'name' property
-                      style={{ borderColor: 'rgba(240, 248, 255, 0.207)', boxShadow: '0 0 5px #ffffffb2, 0 0 15px #ffffff53, 0 0 20px #ffffff78, 0 0 25px #ffffff82, 0 0 30px #ffffff84' }} 
-                    />
-                    <h4 className='text-white text-center absolute top-1/2 left-0 right-0 -translate-y-1/2 opacity-0 mt-[20px] transition-all duration-300 ease-in'>
-                      {pokemon.name}
-                    </h4>
-                  </div>
-                </Link>
-              </motion.div>
-            );
-          })}
-        </div>
-      </motion.div>
-    )
-  )
-}
+    <>
+      <div className="homepage-bg flex h-full w-full flex-col items-center">
+        <h1>Select your pokemon!</h1>
+        <FilteredList handleSelection={handleSelection} />
+      </div>
+    </>
+  );
+};
 
-export default PokemonSelectionPage
+export default PokemonSelectionPage;
